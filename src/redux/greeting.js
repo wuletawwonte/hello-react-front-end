c8e0b6a1afe468148f8d7aebe998c2c115f9de43
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-09-03 10:20:08 +0300

    Add redux store for greeting

---
+++ src/redux/greeting.js
@@ -0,0 +1,35 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  loading: false,
+  greeting: [],
+  error: '',
+};
+
+export const fetchGreeting = createAsyncThunk('greeting/fetchGreeting', () => fetch('http://localhost:3001/api/v1/greetings')
+  .then((response) => response.json())
+  .then((data) => data));
+
+export const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchGreeting.pending, (state) => {
+      state.loading = true;
+    });
+
+    builder.addCase(fetchGreeting.fulfilled, (state, action) => {
+      state.loading = false;
+      state.greeting = action.payload;
+      state.error = '';
+    });
+
+    builder.addCase(fetchGreeting.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+      state.greeting = [];
+    });
+  },
+});
+
+export default greetingSlice.reducer;
